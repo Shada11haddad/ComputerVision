bdb6b181c74265336b30a613408a318b5ab46b4a
shada <103006276+shadaESQ@users.noreply.github.com>
2025-08-18 11:42:16 +0300

    remove Noise

---
+++ Blurring/removeNoise.py
@@ -0,0 +1,21 @@
+import os
+import cv2
+
+
+img = cv2.imread(os.path.join('.','img','cow-salt-peper.png'))
+
+
+k_size = 7
+
+
+blur_img= cv2.blur(img, (k_size, k_size))
+Gaussian_blur_img= cv2.GaussianBlur(img, (k_size, k_size), 0)
+median_blur_img= cv2.medianBlur(img,k_size)
+
+
+# Display the blurred image
+cv2.imshow('Gaussian Blur Image', Gaussian_blur_img)
+cv2.imshow('median Blur Image', median_blur_img)
+cv2.imshow('Blurred Image', blur_img)
+cv2.imshow('Original Image', img)
+cv2.waitKey(0)
